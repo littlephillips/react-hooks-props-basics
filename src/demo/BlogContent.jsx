a38c72cbdcd71b355b969f37b16f871832e50efb
littlephillips <phillipkinuthia72@gmail.com>
2022-09-21 10:54:11 +0300

    code along

---
+++ src/demo/BlogContent.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+function BlogContent(props) {
+  if(!props.isPublished){
+    return null;
+  } else {
+    return (
+      <div>
+        <h1>{props.articleText}</h1> 
+        <p>{props.minutesToRead} minutes to read</p>
+      </div>
+    );
+  }  
+} 
+export default BlogContent;
---
+++ src/demo/BlogPost.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import BlogContent from './BlogContent'
+
+function BlogPost() {
+  return (
+    <div>
+      <BlogContent articleText ="Dear Reader"/>
+    </div>
+  )
+}
+
+export default BlogPost
